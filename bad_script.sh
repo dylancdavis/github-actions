200681979bf519556abf1690ba7197bfcc522e9c
Cole D <104338788+dylancdavis@users.noreply.github.com>
2025-03-05 12:58:40 -0500

    Fix bad_script.sh

--- bad_script.sh
+++ bad_script.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-## Example: a typical script with several problems
-for f in $(ls *.m3u)
+## Applied fixes and linting
+for f in *.m3u
 do
-  grep -qi hq.*mp3 $f \
-    && echo -e 'Playlist $f contains a HQ file in mp3 format'
+  [[ -e $f ]] || break
+  grep -qi "hq.*mp3" $f \
+    && echo "Playlist $f contains a HQ file in mp3 format"
 done
