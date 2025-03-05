1de1613b54ee43b6c99e1133b90a28398ece5d5d
Cole D <104338788+dylancdavis@users.noreply.github.com>
2025-03-05 13:02:49 -0500

    Update bad_script.sh

--- bad_script.sh
+++ bad_script.sh
@@ -2,7 +2,7 @@
 ## Applied fixes and linting
 for f in *.m3u
 do
-  [[ -e $f ]] || break
-  grep -qi "hq.*mp3" $f \
+  [ -e "$f" ] || break
+  grep -qi "hq.*mp3" "$f" \
     && echo "Playlist $f contains a HQ file in mp3 format"
 done
